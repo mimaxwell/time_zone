a78ebc347f607516020754493ce77b69f9f6596c
mimaxwell <mimaxwell@ymail.com>
2020-09-13 23:35:32 +0000

    get countries endpoint working

--- time_project/time_app/views.py
+++ time_project/time_app/views.py
@@ -86,10 +86,9 @@ class ZoneTimeOffset(generics.ListAPIView):
         return results
 
 
-# TODO this is unfinished while I work on an endpoint that 
-# returns the offset from the name
 # this doesn't have a serializer yet
 # endpoint "works" but returns wrong thing
+# example: /time/zone/BET/countries
 class ZoneTimeCountries(views.APIView):
     # TODO explain this 
     sys.stdout.reconfigure(encoding='utf-8')
@@ -103,22 +102,6 @@ class ZoneTimeCountries(views.APIView):
         # offset is in the form of <QuerySet [('GMT-3:00',)]>
         # only need it to be -3:00
         offset_gmt = ''.join(list(offset_queryset)[0])
-        offset_utc = offset_gmt.replace('GMT', 'UTC') # now in format UTC-3:00
-        # these come in the wrong format. The easiest thing to do would be to 
-        # alter the csv file and reload it in to the database.
-        # I wonder what other endpoints this will break?
-
-        ## NEXT STEPS: in the django shell this is returned: 
-        # >>> TimeZone.objects.filter(name='EET').values_list('utc_offset')
-        # <QuerySet [('GMT+2:00',), ('GMT+02:00',), ('UTC+02:00',)]>
-        # when I alter and upload the csv it is simply tacking on those values, not overwriting them
-        # figure out how to programatically delete all the entries except for the last one
-        # while I could compensate in code that will make it impossible for other to 
-        # use my code
-        new_offset = "'" + offset_utc + "'"
-        var = ['UTC-03:00', 'UTC+3:00']
-        print(new_offset)
-        print('UTC-03:00' in var)
 
         response = requests.get('https://restcountries.eu/rest/v2/all')        
         parsed = json.loads(response.text)
@@ -127,22 +110,15 @@ class ZoneTimeCountries(views.APIView):
         # names where [entry]["timezones"] has an element that == offset_utc
         # collect those names in a list. return that list
 
-
+        countries_list = []
         # begin just by returning all country names and their timezones
         #iterate through the entries
         for entry in range(len(parsed)):
-            if (offset_utc in parsed[entry]["timezones"] ):
-                print(parsed[entry]["name"])
-                print(parsed[entry]["timezones"])
-            #print(type(parsed[entry]["timezones"]))
-            #print(parsed[entry]["name"])
-            #print(parsed[entry]["timezones"])
-        
-        # then will need to filter by offset
-        # so from the three digit code get the associated offset
-        # if that offset matches return the country name
-        var = [{"countries": parsed}]
-        results = ZoneTimeCountriesSerializer(var, many=True).data
+            if (offset_gmt in parsed[entry]["timezones"] ):
+                countries_list.append(parsed[entry]["name"])
+
+
+        results = ZoneTimeCountriesSerializer([{"countries": countries_list}], many=True).data
         
         return Response(results)
 
