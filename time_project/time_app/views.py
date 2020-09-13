2b7bbc3dc6e43fdfca26c9165f7f6373c2a03ce1
mimaxwell <mimaxwell@ymail.com>
2020-09-13 23:12:28 +0000

    fix endpoints to work with new database values

--- time_project/time_app/views.py
+++ time_project/time_app/views.py
@@ -34,6 +34,7 @@ class ZoneTime(generics.ListCreateAPIView):
 
 # want a view that is for individual rows in timezone model
 # accessed by pk in path
+# time/zone/7/ 
 class ZoneTimeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = TimeZone.objects.all()
     serializer_class = ZoneTimeSerializer
@@ -43,16 +44,34 @@ class ZoneTimeDetail(generics.RetrieveUpdateDestroyAPIView):
 # returns time but not offset
 class ZoneTimeName(generics.ListAPIView):
     serializer_class = ZoneTimeNameSerializer
+
     def get_queryset(self):
         name = self.kwargs['name']
 
         # A whole bunch of nasty type conversion
-        gmt = TimeZone.objects.filter(name=name).values_list('utc_offset')  
-        listgmt = list(gmt)
-        stringgmt = ''.join(listgmt[0])
-        colon_location = stringgmt.find(":")
-        utcoffset_temp = stringgmt[0:colon_location]
-        tz = pytz.timezone('Etc/'+utcoffset_temp)
+        utc = TimeZone.objects.filter(name=name).values_list('utc_offset')
+        #print(gmt) 
+        listutc = list(utc) #convert from query set to list
+        #print(listgmt)
+        stringutc = ''.join(listutc[0]) #convert from list to string
+        gmt = stringutc.replace('UTC', 'GMT')
+        #print(stringgmt)
+        colon_location = gmt.find(":")
+        #print(colon_location) # 6
+        utcoffset_temp = gmt[0:colon_location]
+        #print(utcoffset_temp.index('0',0,5)) #GMT-03
+
+        
+        try: # throws value error if a 0 is not at index 4
+            if (utcoffset_temp.index('0',0,5) == 4):
+                # remove substring
+                new_utcoffset = utcoffset_temp[:4] + utcoffset_temp[5:]
+        except ValueError as ve:
+            new_utcoffset = utcoffset_temp
+        # if it's in the format GMT-03 it needs to be in GMT-3
+        # needs to be in the format 'Etc/GMT-3
+        # currently in the format UTC-03:00
+        tz = pytz.timezone('Etc/'+new_utcoffset)
 
         time = [{"time" : pytz.utc.localize(datetime.datetime.utcnow()).astimezone(tz)}]
         results = ZoneTimeNameSerializer(time, many=True).data
@@ -91,11 +110,26 @@ class ZoneTimeCountries(views.APIView):
         # only need it to be -3:00
         offset_gmt = ''.join(list(offset_queryset)[0])
         offset_utc = offset_gmt.replace('GMT', 'UTC') # now in format UTC-3:00
+        # these come in the wrong format. The easiest thing to do would be to 
+        # alter the csv file and reload it in to the database.
+        # I wonder what other endpoints this will break?
+
+        ## NEXT STEPS: in the django shell this is returned: 
+        # >>> TimeZone.objects.filter(name='EET').values_list('utc_offset')
+        # <QuerySet [('GMT+2:00',), ('GMT+02:00',), ('UTC+02:00',)]>
+        # when I alter and upload the csv it is simply tacking on those values, not overwriting them
+        # figure out how to programatically delete all the entries except for the last one
+        # while I could compensate in code that will make it impossible for other to 
+        # use my code
+        new_offset = "'" + offset_utc + "'"
+        var = ['UTC-03:00', 'UTC+3:00']
+        print(new_offset)
+        print('UTC-03:00' in var)
 
         response = requests.get('https://restcountries.eu/rest/v2/all')        
         parsed = json.loads(response.text)
     
-        # NEXT STEPS: now that I have the offset use that to find the country 
+        # now that I have the offset use that to find the country 
         # names where [entry]["timezones"] has an element that == offset_utc
         # collect those names in a list. return that list
 
@@ -103,8 +137,12 @@ class ZoneTimeCountries(views.APIView):
         # begin just by returning all country names and their timezones
         #iterate through the entries
         for entry in range(len(parsed)):
-            print(parsed[entry]["name"])
-            print(parsed[entry]["timezones"])
+            if (offset_utc in parsed[entry]["timezones"] ):
+                print(parsed[entry]["name"])
+                print(parsed[entry]["timezones"])
+            #print(type(parsed[entry]["timezones"]))
+            #print(parsed[entry]["name"])
+            #print(parsed[entry]["timezones"])
         
         # then will need to filter by offset
         # so from the three digit code get the associated offset
